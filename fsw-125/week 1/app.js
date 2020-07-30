672cc24425295528ec22d120c5a7420688e2e76c
melissarvogel <63073544+melissarvogel@users.noreply.github.com>
2020-07-30 11:09:37 -0400

    Add files via upload

---
+++ fsw-125/week 1/app.js
@@ -0,0 +1,11 @@
+const math = require("./math.js")
+
+console.log(math.sum(10, 20))
+
+console.log(math.sub(30, 10))
+
+console.log(math.mult(10, 20))
+
+console.log(math.div(40, 2))
+
+
---
+++ fsw-125/week 1/math.js
@@ -0,0 +1,24 @@
+function sum(a, b){
+    return (a + b)
+}
+
+function sub(c, d){
+    return (c - d) 
+    }
+
+function mult(e, f){
+    return (e * f)
+}
+
+function div(g, h){
+    return(g/h)
+}
+
+module.exports = {
+    sum: sum,
+    sub: sub,
+    mult: mult,
+    div: div
+}
+
+
